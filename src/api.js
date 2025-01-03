d02483ae06583b1971d0d2a606e3e3fe11cf837c
oo11boy <editkhone@gmail.com>
2025-01-03 20:22:40 +0330

    update

--- src/api.js
+++ src/api.js
@@ -1,12 +1,4 @@
 export let Api ='http://localhost:5000'
 
 
-//   http://localhost:5000
-
-// https://apishop-md.vercel.app
-
-export let domain='http://localhost:3001/'
-
-// https://shopnew-tau.vercel.app/
-
-//http://localhost:3000/
+// export let domain='http://localhost:3001/'
